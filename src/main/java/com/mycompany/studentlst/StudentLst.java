c88b3b1ed1487c4a249ddebcdc25fca341220a00
Olha Sorych <soryo@farmingdale.edu>
2024-09-02 14:06:24 -0400

    Completing the implementation of printOutput() method

--- src/main/java/com/mycompany/studentlst/StudentLst.java
+++ src/main/java/com/mycompany/studentlst/StudentLst.java
@@ -10,25 +10,27 @@ public class StudentLst {
     public static void main(String[] args) {
         Student[] students= new Student[2];
         Scanner inScanner = new Scanner(System.in);
-        
+
         for(int i=0; i< students.length;i++){
             System.out.print("Enter student's name: ");
             String name = inScanner.nextLine();
             Student student = new Student(name);
-            
+
             students[i]= student;
-            
+
         }
         System.out.println(printOutput(students));
     }
-    
+
     public static String printOutput(Student[] students){
         String str="";
-        
-        //To Do 03: complete the implementation of this method  
-        
-        return str;
+
+        //To Do 03: complete the implementation of this method
+        for(int i=0; i<students.length;i++) {
+            str += (i+1) + ". " + students[i].getName() + ", ";
+        }
+        return str + "\b\b";
     }
-    
-    
+
+
 }
